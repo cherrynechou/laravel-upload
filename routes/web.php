e99178f91a5b413f884a2812b01fa756fab079f4
cherrynechou <iboy_2008@163.com>
2020-11-15 21:45:17 +0800

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::group([
     'prefix'        =>  config('admin.route.prefix')
 ], function (Router $router) {
 
-    Route::get('utils/getResources', Controllers\UtilsController::class.'@getResources')->name('api.utils.getResources');
-    Route::get('utils/getCategories', Controllers\UtilsController::class.'@getCategories')->name('api.utils.getCategories');
-    Route::post('resource/postUpload', Controllers\UploadController::class.'@postUpload')->name('api.resource.postUpload');
-    Route::post('resource/destroy',Controllers\UploadController::class.'@destroy')->name('api.resource.destroy');
+    Route::get('utils/getResources', Controllers\UtilsController::class.'@getResources')->name('api.admin.utils.getResources');
+    Route::get('utils/getCategories', Controllers\UtilsController::class.'@getCategories')->name('api.admin.utils.getCategories');
+    Route::post('resource/postUpload', Controllers\UploadController::class.'@postUpload')->name('api.admin.resource.postUpload');
+    Route::post('resource/destroy',Controllers\UploadController::class.'@destroy')->name('api.admin.resource.destroy');
 
 });
